ae8ef56ad7ff55f60b595b362f597c1c5f6176a8
Nard <Nard@macbook-pro-van-nard.local>
2015-12-03 11:45:33 +0100

    Initial Commit

---
+++ navigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  navigation
+//
+//  Created by Nard Broekstra on 03-12-15.
+//  Copyright © 2015 P-App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
